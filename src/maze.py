67f71bc79bd38fe9aa22f3b96a24fc2f6407d1a8
Alexandr Tabara <tabarale@fit.cvut.cz>
2024-04-17 20:16:19 +0200

    Outline of load_maze_csv

---
+++ src/maze.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def load_maze_csv(file_name):
+    data = np.genfromtxt(file_name, delimiter=",", dtype=bool)
+    return data
+
+
+def find_shortest_path():
+    pass
+
+
+def generate_maze():
+    pass
