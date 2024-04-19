5c0902fbe3cdedd0ddd134f177f98ad463497bc5
Alexandr Tabara <tabarale@fit.cvut.cz>
2024-04-19 09:01:42 +0200

    Create Maze class

--- src/maze.py
+++ src/maze.py
@@ -1,14 +1,28 @@
 import numpy as np
 
 
+class Maze:
+    def __init__(self, data):
+        self.data = data
+
+    def incidence_matrix(self):
+        pass
+
+    def bfs(self):
+        pass
+
+    def find_shortest_path(self):
+        pass
+
+
 def load_maze_csv(file_name):
-    data = np.genfromtxt(file_name, delimiter=",", dtype=bool)
-    return data
-
-
-def find_shortest_path():
-    pass
+    data = np.genfromtxt(file_name, delimiter=",")
+    return data.astype(bool)
 
 
 def generate_maze():
     pass
+
+
+data = load_maze_csv("./data/maze_5.csv")
+print(data)
