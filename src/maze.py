38cc2aa496d850d38ddbab78fcdd4844f023d1c1
Alexandr Tabara <tabarale@fit.cvut.cz>
2024-04-19 19:49:19 +0200

    Add incidence matrix

--- src/maze.py
+++ src/maze.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.sparse as sparse
 
 
 class Maze:
@@ -7,7 +8,27 @@ class Maze:
         self.data = data
 
     def incidence_matrix(self):
-        pass
+        n, m = self.data.shape
+        A = sparse.lil_matrix((n * n, n * n))
+
+        for i in range(n):
+            for j in range(n):
+                idx = i * m + j
+
+                if i > 0 and self.data[i - 1, j] == 0:  # upper neighbour
+                    A[idx, idx - n] = 1
+
+                if i < n - 1 and self.data[i + 1, j] == 0:  # lower neighbour
+                    A[idx, idx + n] = 1
+
+                if j > 0 and self.data[i, j - 1] == 0:  # left neighbour
+                    A[idx, idx - 1] = 1
+
+                if j < m - 1 and self.data[i, j + 1] == 0:  # right neighbour
+                    A[idx, idx + 1] = 1
+
+                A[idx, idx] = 0
+        return A
 
     def bfs(self):
         pass
@@ -34,3 +55,10 @@ data = load_maze_csv("./data/maze_5.csv")
 print(data)
 maze = Maze(data)
 maze.draw_maze()
+
+indicence_matrix = maze.incidence_matrix()
+print(indicence_matrix)
+
+plt.figure()
+plt.imshow(indicence_matrix.todense(), cmap="binary")
+plt.show()
